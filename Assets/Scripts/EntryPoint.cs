4ac4d254c5f35474415f0b230a8683a7ab9e5c01
Vladislav529 <67689833+Vladislav529@users.noreply.github.com>
2021-11-17 01:42:03 +0300

    Main menu

---
+++ Assets/Scripts/EntryPoint.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EntryPoint : MonoBehaviour
+{
+    public WindowManager windowManager;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        windowManager.ShowWindow("BaseWindow");
+    }
+}
