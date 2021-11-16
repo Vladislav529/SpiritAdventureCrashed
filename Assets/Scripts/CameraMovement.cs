7b36ff8d5ee5f996a576b3305f3b120e9602663b
Max <94285474+N4palm1337@users.noreply.github.com>
2021-11-17 01:47:12 +0300

    aboba bebra

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -3,9 +3,15 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour
 {
-    
+
+    [Header("Set in Inspector")]
+    public GameObject character;
+    public GameObject camera;
+
     private void LateUpdate()
     {
-        
+        Vector3 characterPos = character.transform.position;
+
+        Camera.main.transform.position = new Vector3(characterPos.x, characterPos.y, Camera.main.transform.position.z);
     }
 }
