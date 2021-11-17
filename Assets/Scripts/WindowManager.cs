012ce075460bbb8d30837d9c909e84ef41aab782
Vladislav529 <67689833+Vladislav529@users.noreply.github.com>
2021-11-17 21:13:40 +0300

    Main Menu Fix

--- Assets/Scripts/WindowManager.cs
+++ Assets/Scripts/WindowManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class WindowManager : MonoBehaviour
 {
     public GameObject windowParent;
+    private List<BaseWindow> openedWindows = new List<BaseWindow>();
 
     public void ShowWindow(string prefabName)
     {
@@ -13,10 +14,30 @@ public class WindowManager : MonoBehaviour
         var window = GameObject.Instantiate<BaseWindow>(prototype, windowParent.transform);
         window.Init(this);
         window.closeEvent.AddListener(CloseWindow);
+        if (openedWindows.Count > 0)
+        {
+            CollapseWindow(openedWindows[openedWindows.Count - 1]);
+        }
+        openedWindows.Add(window);
     }
 
     public void CloseWindow(BaseWindow window)
     {
         Destroy(window.gameObject);
+        openedWindows.Remove(window);
+        if (openedWindows.Count > 0)
+        {
+            ExpandWindow(openedWindows[openedWindows.Count - 1]);
+        }
+    }
+
+    public void ExpandWindow(BaseWindow window)
+    {
+        window.gameObject.SetActive(true);
+    }
+
+    public void CollapseWindow(BaseWindow window)
+    {
+        window.gameObject.SetActive(false);
     }
 }
