4b9b7874ee2c34cf9ff90a9ec6e86f51b4547333
Vladislav529 <67689833+Vladislav529@users.noreply.github.com>
2021-11-18 01:22:13 +0300

    Options Fix

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +13,16 @@ public class Options : BaseWindow
     public float volume = 0; //Громкость
     public int quality = 0; //Качество
     public bool isFullscreen = false; //Полноэкранный режим
-    public AudioMixer audioMixer; //Регулятор громкости
+	private int currResolutionIndex = 0; //Текущее разрешение
+
+	public AudioMixer audioMixer; //Регулятор громкости
     public Dropdown resolutionDropdown; //Список с разрешениями для игры
     private Resolution[] resolutions; //Список доступных разрешений
-    private int currResolutionIndex = 0; //Текущее разрешение
 
-    public void ChangeVolume(float val) //Изменение звука
+	public Button saveButton;
+	public Button exitButton;
+
+	public void ChangeVolume(float val) //Изменение звука
     {
         volume = val;
     }
@@ -32,33 +39,122 @@ public class Options : BaseWindow
         quality = index;
     }
 
-    public void SaveSettings()
-    {
-        audioMixer.SetFloat("MasterVolume", volume); //Изменение уровня громкости
-        QualitySettings.SetQualityLevel(quality); //Изменение качества
-        Screen.fullScreen = isFullscreen; //Включение или отключение полноэкранного режима
-        Screen.SetResolution(Screen.resolutions[currResolutionIndex].width, Screen.resolutions[currResolutionIndex].height, isFullscreen); //Изменения разрешения
-    }
+	public override void Awake()
+	{
+		Debug.Log("Options.Awake()");
+		base.Awake();
+		LoadSettings();
+		saveButton.onClick.AddListener(SaveSettings);
+		exitButton.onClick.AddListener(ReturnToMain);
+	}
 
-    public void AutoSettings()
-    {
-        resolutionDropdown.ClearOptions(); //Удаление старых пунктов
-        resolutions = Screen.resolutions; //Получение доступных разрешений
-        List<string> options = new List<string>(); //Создание списка со строковыми значениями
+	private void ReturnToMain()
+	{
+		_windowManager.CloseWindow(this);
+	}
 
-        for (int i = 0; i < resolutions.Length; i++) //Поочерёдная работа с каждым разрешением
+	public void SaveSettings()
+	{
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(Application.persistentDataPath + "/Settings.dat");
+		SaveData data = new SaveData();
+		data.savedQuality = quality;
+		data.savedResolution = currResolutionIndex;
+		data.savedVolume = volume;
+		data.savedFullscreen = isFullscreen;
+		bf.Serialize(file, data);
+		file.Close();
+		Debug.Log("Game data saved!");
+	}
+
+	public void LoadSettings()
+	{
+		if (File.Exists(Application.persistentDataPath + "/Settings.dat"))
+		{
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/Settings.dat", FileMode.Open);
+			SaveData data = (SaveData)bf.Deserialize(file);
+			file.Close();
+			quality = data.savedQuality;
+			currResolutionIndex = data.savedResolution;
+			volume = data.savedVolume;
+			isFullscreen = data.savedFullscreen;
+			audioMixer.SetFloat("Volume", volume); //Изменение уровня громкости
+			QualitySettings.SetQualityLevel(quality); //Изменение качества
+			Screen.fullScreen = isFullscreen; //Включение или отключение полноэкранного режима
+			Screen.SetResolution(Screen.resolutions[currResolutionIndex].width, Screen.resolutions[currResolutionIndex].height, isFullscreen); //Изменения разрешения
+			Debug.Log("Game data loaded!");
+		}
+		else
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height; //Создание строки для списка
-            options.Add(option); //Добавление строки в список
-
-            if (resolutions[i].Equals(Screen.currentResolution)) //Если текущее разрешение равно проверяемому
-            {
-                currResolutionIndex = i; //То получается его индекс
-            }
-        }
-
-        resolutionDropdown.AddOptions(options); //Добавление элементов в выпадающий список
-        resolutionDropdown.value = currResolutionIndex; //Выделение пункта с текущим разрешением
-        resolutionDropdown.RefreshShownValue(); //Обновление отображаемого значения
-    }
+			Debug.LogError("There is no save data!");
+		}
+	}
 }
+
+[Serializable]
+public class SaveData
+{
+	public int savedQuality;
+	public int savedResolution;
+	public float savedVolume;
+	public bool savedFullscreen;
+}
+
+//public class Options2 : BaseWindow
+//{
+//	public float volume = 0; //Громкость
+//	public int quality = 0; //Качество
+//	public bool isFullscreen = false; //Полноэкранный режим
+//	public AudioMixer audioMixer; //Регулятор громкости
+//	public Dropdown resolutionDropdown; //Список с разрешениями для игры
+//	private Resolution[] resolutions; //Список доступных разрешений
+//	private int currResolutionIndex = 0; //Текущее разрешение
+
+//	public void ChangeVolume(float val) //Изменение звука
+//	{
+//		volume = val;
+//	}
+//	public void ChangeResolution(int index) //Изменение разрешения
+//	{
+//		currResolutionIndex = index;
+//	}
+//	public void ChangeFullscreenMode(bool val) //Включение или отключение полноэкранного режима
+//	{
+//		isFullscreen = val;
+//	}
+//	public void ChangeQuality(int index) //Изменение качества
+//	{
+//		quality = index;
+//	}
+
+//	public void SaveSettings()
+//	{
+//		audioMixer.SetFloat("MasterVolume", volume); //Изменение уровня громкости
+//		QualitySettings.SetQualityLevel(quality); //Изменение качества
+//		Screen.fullScreen = isFullscreen; //Включение или отключение полноэкранного режима
+//		Screen.SetResolution(Screen.resolutions[currResolutionIndex].width, Screen.resolutions[currResolutionIndex].height, isFullscreen); //Изменения разрешения
+//	}
+
+//	public void AutoSettings()
+//	{
+//		resolutionDropdown.ClearOptions(); //Удаление старых пунктов
+//		resolutions = Screen.resolutions; //Получение доступных разрешений
+//		List<string> options = new List<string>(); //Создание списка со строковыми значениями
+
+//		for (int i = 0; i < resolutions.Length; i++) //Поочерёдная работа с каждым разрешением
+//		{
+//			string option = resolutions[i].width + " x " + resolutions[i].height; //Создание строки для списка
+//			options.Add(option); //Добавление строки в список
+
+//			if (resolutions[i].Equals(Screen.currentResolution)) //Если текущее разрешение равно проверяемому
+//			{
+//				currResolutionIndex = i; //То получается его индекс
+//			}
+//		}
+
+//		resolutionDropdown.AddOptions(options); //Добавление элементов в выпадающий список
+//		resolutionDropdown.value = currResolutionIndex; //Выделение пункта с текущим разрешением
+//		resolutionDropdown.RefreshShownValue(); //Обновление отображаемого значения
+//	}
+//}
